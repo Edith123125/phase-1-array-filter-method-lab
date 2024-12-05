1d6945a991d0b37926b11108b9a7856044f45831
Edith Gatwiri <edithgatwiri70@gmail.com>
2024-12-05 07:43:41 +0300

    filter

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here
+
+//  findMatching
+function findMatching(names, query1) {
+    return names.filter(names => names.toLowerCase() === query1.toLowerCase());
+}
+
+// fuzzyMatch
+function fuzzyMatch(names, query2) {
+    return names.filter(name => name.startsWith(query2));
+}
+
+// matchName
+function matchName(driverObjects, query3) {
+    return driverObjects.filter(driver => driver.name === query3);
+}
+
+const names = ["Sally", "Sam", "Sara", "Ann"];
+const driverObjects = [
+    { name: "Sally", hometown: "New York" },
+    { name: "Sam", hometown: "Los Angeles" },
+    { name: "Sara", hometown: "Chicago" },
+    { name: "Ann", hometown: "Houston" }
+];
+
+console.log(findMatching(names, "sally")); 
+console.log(fuzzyMatch(names, "Sa"));
+console.log(matchName(driverObjects, "Sam")); 
